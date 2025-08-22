f6187fdd0032ec52cb5479f1e731cf37bcca2376
SureshK2004 <sureshksureshk04gmail.com>
2025-08-22 17:26:44 +0530

    finish

--- src/Components/common/Header.jsx
+++ src/Components/common/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
           <Nav className="ms-auto">
             <Nav.Link href="#services">Services</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#Footer">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
